7e475edc4b57ccc18aad5ec6797055356bf74ace
mhrynovets <hrynovets@gmail.com>
2019-03-25 11:52:38 +0200

    final fix of provisioning

--- task2/azure/vms.tf
+++ task2/azure/vms.tf
@@ -26,7 +26,7 @@ resource "azurerm_virtual_machine" "main-vms" {
     //computer_name  = "${var.prefix}-vm-${count.index}"
     computer_name  = "${element(azurerm_public_ip.main-ip.*.domain_name_label, count.index)}"
     admin_username = "devops"
-    admin_password = "Password1237!"
+    //admin_password = "Password1237!"
   }
   os_profile_linux_config {
     disable_password_authentication = true
@@ -36,32 +36,16 @@ resource "azurerm_virtual_machine" "main-vms" {
     }]
   }
 
-  tags = "${merge(
-    local.common_tags,
-    map(
-      "custom-vm", "${var.prefix}-vm"
-    )
-  )}"
-}
-
-resource "null_resource" "copy-test-file" {
-  count = "${var.vms_count}"
-  
-  triggers {
-    //vms_ids = "${azurerm_virtual_machine.main-vms.*.id}"
-    vms_ids = "${element(azurerm_virtual_machine.main-vms.*.id, count.index)}"
-  }  
-
   connection {
+      user = "devops"
+      private_key = "${tls_private_key.main-tls.private_key_pem}"
       //host = "${element(azurerm_public_ip.main-ip.*.domain_name_label, count.index)}"
       //host = "${element(azurerm_public_ip.main-ip.*.ip_address, count.index)}"
-      host = "${element(azurerm_public_ip.main-ip.*.fqdn, count.index)}"
+      //host = "${element(azurerm_public_ip.main-ip.*.fqdn, count.index)}"
       //host = "${element(data.azurerm_public_ip.main-data-ip.*.ip_address, count.index)}"
-      user = "devops"
-      type = "ssh"
-      private_key = "${tls_private_key.main-tls.private_key_pem}"
+      //type = "ssh"
       //timeout = "2m"
-      agent = true
+      //agent = true
   }
 
   provisioner "file" {
@@ -75,9 +59,11 @@ resource "null_resource" "copy-test-file" {
       "/tmp/initweb.sh",
     ]
   }
-  depends_on = [
-    "azurerm_virtual_machine.main-vms", 
-    "data.azurerm_public_ip.main-data-ip", 
-    "azurerm_public_ip.main-ip"
-  ]
+
+  tags = "${merge(
+    local.common_tags,
+    map(
+      "custom-vm", "${var.prefix}-vm"
+    )
+  )}"
 }
