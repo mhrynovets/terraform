5cbaa4dadde35edb30111123d04a11280628355f
mhrynovets <hrynovets@gmail.com>
2019-03-28 12:33:32 +0200

    minor: mod AKV generation script

--- task2/azure/create_secrets/get-vault/main.tf
+++ task2/azure/create_secrets/get-vault/main.tf
@@ -1,14 +1,14 @@
 data "azurerm_key_vault" "keyvault" {
-  name                = "tf-data-kv"
+  name                = "tf-data-kv-2"
   resource_group_name = "tf-service"
 }
 
-data "azurerm_key_vault_secret" "test" {
+data "azurerm_key_vault_secret" "ssh_pub" {
   name      = "vms-ssh-pub"
-  vault_uri = "${data.azurerm_key_vault.keyvault.vault_uri}"
+  // deprecated: vault_uri = "${data.azurerm_key_vault.keyvault.vault_uri}"
+  key_vault_id = "${data.azurerm_key_vault.keyvault.id}"
 }
 
 output "secret_value" {
-  value = "${data.azurerm_key_vault_secret.test.value}"
-
+  value = "${data.azurerm_key_vault_secret.ssh_pub.value}"
 }
--- task2/azure/create_secrets/main.tf
+++ task2/azure/create_secrets/main.tf
@@ -6,7 +6,7 @@ locals{
 }
 
 resource "azurerm_key_vault" "test" {
-  name                = "tf-data-kv"
+  name                = "tf-data-kv-2"
   location            = "${local.location}"
   resource_group_name = "${local.rg-name}"
   tenant_id           = "${data.azurerm_client_config.current.tenant_id}"
